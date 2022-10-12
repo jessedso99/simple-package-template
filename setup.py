4bdaea57ba654743796d1ea195582bd95322f54a
jessedso99 <59036618+jessedso99@users.noreply.github.com>
2022-10-12 16:43:04 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="package_name",
+    version="0.0.2",
+    author="jessw99",
+    author_email="jessedsoliveira@gmail.com",
+    description="My short description",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jessedso99/simple-package-template",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
